aa08a67ba06778e016534d49feaf31627059bcc8
Frederick Lahde <frederick.lahde@worldiety.de>
2023-12-19 15:16:45 +0100

    bumb version

--- magic.go
+++ magic.go
@@ -14,7 +14,7 @@
 
 package main
 
-const version = "0.0.22"
+const version = "0.0.23"
 const goUp = "GoUp"
 const goup = "goup"
 const defaultResourcesURL = "https://raw.githubusercontent.com/worldiety/goup/master/resources.xml"
